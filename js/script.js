f1a0f236c1be310d2c8401d6d613d2b33d967fec
Irina Kovalchuk <synthbeing@icloud.com>
2019-06-06 10:23:33 +0300

    Minor criterias fixes

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
-window.addEventListener('DOMContentLoaded', function () {
-    let btnFeedback = document.getElementById('feedback-btn');
-    let elFeedback = document.getElementById('feedback');
+window.addEventListener("DOMContentLoaded", function () {
+    let btnFeedback = document.getElementById("feedback-btn");
+    let elFeedback = document.getElementById("feedback");
     let btnCloseFeedback = document.getElementById("btn-close-feedback");
-    let inputFeedbackName = document.querySelectorAll('.modal-feedback input')[0];
-    let elOverlay = document.getElementById('overlay');
+    let inputFeedbackName = document.querySelectorAll(".modal-feedback input")[0];
+    let elOverlay = document.getElementById("overlay");
 
-    btnFeedback.addEventListener('click', function (event) {
+    btnFeedback.addEventListener("click", function (event) {
         event.preventDefault();
-        elFeedback.classList.add('modal--show');
-        elFeedback.classList.remove('modal--hide');
-        elOverlay.classList.add('overlay--show');
+        elFeedback.classList.add("modal--show");
+        elFeedback.classList.remove("modal--hide");
+        elOverlay.classList.add("overlay--show");
         inputFeedbackName.focus();
     });
 
-    btnCloseFeedback.addEventListener('click', function () {
-        elFeedback.classList.remove('modal--show');
-        elFeedback.classList.add('modal--hide');
-        elOverlay.classList.remove('overlay--show');
+    btnCloseFeedback.addEventListener("click", function () {
+        elFeedback.classList.remove("modal--show");
+        elFeedback.classList.add("modal--hide");
+        elOverlay.classList.remove("overlay--show");
     });
 
-    document.addEventListener('keydown', function (event) {
+    document.addEventListener("keydown", function (event) {
         if (event.keyCode === 27) {
-            elFeedback.classList.remove('modal--show');
-            elFeedback.classList.add('modal--hide');
+            elFeedback.classList.remove("modal--show");
+            elFeedback.classList.add("modal--hide");
         }
     });
 
     // slide-show
-    let slides = document.querySelectorAll('.slide');
-    let dots = document.querySelectorAll('.slider-dot-item');
+    let slides = document.querySelectorAll(".slide");
+    let dots = document.querySelectorAll(".slider-dot-item");
     let backgrounds = {
         0: '#849D8F',
         1: '#8996A6',
@@ -36,48 +36,48 @@ window.addEventListener('DOMContentLoaded', function () {
     };
 
     for (let i = 0; i < dots.length; i++) {
-        dots[i].addEventListener('click', function () {
+        dots[i].addEventListener("click", function () {
             removeActiveStatusFromDots();
-            dots[i].classList.add('slider-dot-item--current');
+            dots[i].classList.add("slider-dot-item--current");
             removeActiveStatusFromSlides();
-            slides[i].classList.add('slide--active');
+            slides[i].classList.add("slide--active");
             document.body.style.backgroundColor = backgrounds[i];
         });
     }
 
     function removeActiveStatusFromDots() {
         for (let i = 0; i < dots.length; i++) {
-            dots[i].classList.remove('slider-dot-item--current');
+            dots[i].classList.remove("slider-dot-item--current");
         }
     }
 
     function removeActiveStatusFromSlides() {
         for (let i = 0; i < slides.length; i++) {
-            slides[i].classList.remove('slide--active');
+            slides[i].classList.remove("slide--active");
         }
     }
 
     //map
     ymaps.ready(init);
     function init() {
-        let myMap = new ymaps.Map('map', {
+        let myMap = new ymaps.Map("map", {
                 center: [59.939300, 30.329350],
                 zoom: 16,
                 controls: []
             }, {
-                searchControlProvider: 'yandex#search'
+                searchControlProvider: "yandex#search"
             }),
 
             MyIconContentLayout = new ymaps.templateLayoutFactory.createClass(
-                '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+                "<div style='color: #FFFFFF; font-weight: bold;'>$[properties.iconContent]</div>"
             ),
 
             myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
-                hintContent: '',
-                balloonContent: ''
+                hintContent: "",
+                balloonContent: ""
             }, {
-                iconLayout: 'default#image',
-                iconImageHref: 'img/icons/interactive-map-pin.svg',
+                iconLayout: "default#image",
+                iconImageHref: "img/icons/interactive-map-pin.svg",
                 iconImageSize: [218, 142],
                 iconImageOffset: [-40, -140]
             });
