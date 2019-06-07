9c0ba1249ca30d39d75ff0d1dc0bb8ca81187521
Irina Kovalchuk <synthbeing@icloud.com>
2019-06-07 14:14:42 +0300

    Custom radio buttons accessibility

--- js/script-accessibility.js
+++ js/script-accessibility.js
@@ -22,4 +22,38 @@ window.addEventListener("DOMContentLoaded", function () {
                 break;
         }
     }
+
+    let radioFat = document.querySelectorAll(".filter-fat .radio");
+
+    for (let i = 0; i < radioFat.length; i++) {
+        radioFat[i].addEventListener("keydown", function (evt) {
+            if (evt.key === "ArrowRight") {
+                evt.preventDefault();
+                if (i !== radioFat.length - 1) {
+                    changeCurrentRadioButton(radioFat[i], radioFat[i].querySelector("input"));
+                    setFocusOnNextOrPreviousItem(radioFat[i + 1], radioFat[i + 1].querySelector("input"));
+                }
+            }
+            if (evt.key === "ArrowLeft") {
+                evt.preventDefault();
+                if (i !== 0) {
+                    changeCurrentRadioButton(radioFat[i], radioFat[i].querySelector("input"));
+                    setFocusOnNextOrPreviousItem(radioFat[i - 1], radioFat[i - 1].querySelector("input"));
+                }
+            }
+        });
+    }
+
+function changeCurrentRadioButton(elRadioButton, elRadioInput) {
+        elRadioButton.setAttribute("tabindex", "-1");
+        elRadioButton.setAttribute("aria-checked", "false");
+        elRadioInput.removeAttribute("checked");
+    }
+
+    function setFocusOnNextOrPreviousItem(elNext, elRadioInput) {
+        elNext.setAttribute("tabindex", "0");
+        elNext.setAttribute("aria-checked", "true");
+        elNext.focus();
+        elRadioInput.setAttribute("checked", "checked");
+    }
 });
